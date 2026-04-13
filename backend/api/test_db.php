be7ba8a3c51f66d4b2637eb84e0c5947ea78a931
micaiahw5360-phub <micaiahw5360@tamcc.edu.gd>
2026-04-12 22:15:34 -0400

    fixed 1

--- backend/api/test_db.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'config/db.php';
-$db = new Database();
-$conn = $db->getConnection();
-if($conn) echo "Connected to database successfully!";
-else echo "Connection failed.";
-?>
